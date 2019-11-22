c2b5f466815c2e5cf69c16b7efde4c747e5952a6
mmach900 <m.mach.90@gmail.com>
2019-11-22 23:25:13 +0100

    no message

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ amqp.connect(process.env.AMQP ? process.env.AMQP : 'amqp://mq2-justshare.e4ff.pr
         var queue = 'olx-sitemap-crawler';
 
 
-        channel.prefetch(5);
+        channel.prefetch(3);
 
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
         channel.consume(queue, function (msg) {
